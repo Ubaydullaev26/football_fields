cc28275e152574af0a8cf0ab04f4a95eb63d04d8
aziz <az.ubaydullaev03@gmail.com>
2024-10-30 01:13:58 +0500

    added swagger

--- football_booking/urls.py
+++ football_booking/urls.py
@@ -5,9 +5,24 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from rest_framework import permissions
 from fields.views import FieldDetailView
 from bookings.views import BookingDetailView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Fastfood API",
+      default_version='v1',
+      description="Документация для API фастфуд ресторана",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="support@fastfood.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +33,6 @@ urlpatterns = [
     path('api/users/', include('users.urls')),  # Optional
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
